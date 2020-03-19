0f6f76f36fb2fd8e0e9f28c82da7816e16b2a1d3
Dmitrii Ninyuk <dimaninyuk@gmail.com>
2020-03-19 21:15:58 +0200

    Asos

--- ComparisonShoppingWebsite/Controllers/ProductsController.cs
+++ ComparisonShoppingWebsite/Controllers/ProductsController.cs
@@ -19,25 +19,50 @@ namespace ComparisonShoppingWebsite.Controllers
         }
 
 
-        [HttpGet, Route("get")]
+        [HttpGet, Route("getAmazon")]
         public IEnumerable<Product> GetAmazon(string keywords)
         {
-            var client = new RestClient("https://amazon-price1.p.rapidapi.com/search?keywords="+keywords+"&marketplace=ES");
+            var client = new RestClient("https://amazon-price1.p.rapidapi.com/search?keywords=iphone&marketplace=ES");
             var request = new RestRequest(Method.GET);
             request.AddHeader("x-rapidapi-host", "amazon-price1.p.rapidapi.com");
             request.AddHeader("x-rapidapi-key", "aceb22d176msh3d21a7602d172ffp194272jsn55c949386a6b");
             IRestResponse response = client.Execute(request);
             var newProd = JsonConvert.DeserializeObject <List<ProductAmazon>>(response.Content.ToString());
+            
             foreach (var prod in newProd)
             {
+                
                 Product pr = new Product();
+                pr.Id = prod.ASIN;
                 pr.Title = prod.title;
                 pr.Url = prod.detailPageURL;
-                pr.Price = double.Parse(prod.price.Remove(prod.price.IndexOf(' '), prod.price.IndexOf('.')));
-                pr.Currentcy = prod.price.Remove(0, prod.price.IndexOf(' '));
+                pr.Price = double.Parse(prod.price.Substring(4));
+                pr.Currentcy = prod.price.Substring(0,3);
                 data.Add(pr);
             }
             return data;
         }
+        [HttpGet, Route("getAsos")]
+        public IEnumerable<Product> GetAsos(string keywords)
+        {
+            var client = new RestClient("https://asos2.p.rapidapi.com/products/v2/list?country=US&currency=USD&sort=freshness&lang=en-US&q=skirt&sizeSchema=US&offset=0&categoryId=4209&limit=48&store=US");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "asos2.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "aceb22d176msh3d21a7602d172ffp194272jsn55c949386a6b");
+            IRestResponse response = client.Execute(request);
+            var newProd = JsonConvert.DeserializeObject<ProductAsos>(response.Content.ToString());
+            foreach (var prod in newProd.Products)
+            {
+                Product pr = new Product();
+                pr.Id = prod.id.ToString();
+                pr.Title = prod.name;
+                pr.Url = prod.url;
+                pr.Price = prod.price.current.value;
+                pr.Currentcy = prod.price.currency;
+                data.Add(pr);
+            }
+            return data;
+        }
+
     }
 }
