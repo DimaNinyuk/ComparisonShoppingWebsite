2de11abacca66afe9860f97881e9cd0d88004f21
Dmitrii Ninyuk <dimaninyuk@gmail.com>
2020-03-19 13:02:49 +0200

    amazon

--- ComparisonShoppingWebsite/ClientApp/src/App.js
+++ ComparisonShoppingWebsite/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { AmazonHome } from './components/AmazonHome'
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
@@ -13,14 +14,19 @@ import './custom.css'
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
-    return (
+    render() {
+        
+        return (
+            
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route path='/AmazonHome' component={AmazonHome} /> 
         <Route path='/counter' component={Counter} />
         <AuthorizeRoute path='/fetch-data' component={FetchData} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
+
       </Layout>
-    );
+        );
+     
   }
 }
---
+++ ComparisonShoppingWebsite/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComparisonShoppingWebsite.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Currentcy { get; set; }
+        public string Url { get; set; }
+    }
+}
